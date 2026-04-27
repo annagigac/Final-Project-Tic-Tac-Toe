479c33187364bcb4230f7455bce463f32e99f3b6
Anna Gigac <ajg7376@psu.edu>
2026-04-27 19:24:46 -0400

    Created a function that gets player input of row and colums (0-2) with invalid moves implemented aswell

--- FinalProjectCMPSC132TicTacToe.py
+++ FinalProjectCMPSC132TicTacToe.py
@@ -14,3 +14,19 @@ def print_board(board): #display board -- print_board(board)
         if i < 2:
             print(" ---+---+---")
     print()
+
+def get_valid_move(board, player): #row and col player input w/ invalid moves ex. 0-2
+    valid = False
+    while not valid:
+        try:
+            row = int(input(f"Player {player}, enter row 0-2: "))
+            col = int(input(f"Player {player}, enter column 0-2: "))
+            if row < 0 or row > 2 or col < 0 or col > 2:
+                print("That spot is outside the board. Try again.")
+            elif board[row][col] != " ":
+                print("That spot is already taken. Try again.")
+            else:
+                board[row][col] = player
+                valid = True   
+        except ValueError:
+            print("Please enter numbers only.")
