8263dac266b56313ca7030ae6c11f5186c4112ba
Anna Gigac <ajg7376@psu.edu>
2026-04-27 19:23:09 -0400

    Created a function that displayed the board using the print_board(board) function required by the project

--- FinalProjectCMPSC132TicTacToe.py
+++ FinalProjectCMPSC132TicTacToe.py
@@ -5,3 +5,12 @@ def create_board():  #create 3x3 board -- list of lists
         [" ", " ", " "]
     ]
     return board
+
+def print_board(board): #display board -- print_board(board)
+    print()
+    print("  0   1   2")
+    for i in range(3):
+        print(i, board[i][0], "|", board[i][1], "|", board[i][2])
+        if i < 2:
+            print(" ---+---+---")
+    print()
